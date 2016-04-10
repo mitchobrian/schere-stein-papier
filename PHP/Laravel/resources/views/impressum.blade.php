464f1d01dc2fc84724217859138dd00644ce576d
Sven Böhrnsen <flynn23@gmx.de>
2016-04-10 19:41:57 +0200

    Impressum in Blade Form umgeändert + master blade

---
+++ PHP/Laravel/resources/views/impressum.blade.php
@@ -0,0 +1,74 @@
+@extends('master')
+
+@section('title', 'Impressum')
+
+@stop
+
+@section('content')
+
+    <div class="container-fluid">
+    <!-- Impressum
+    ================================================== -->
+    <div class="col-sm-7">
+      <h1 id="kontakt">Impressum</h1>
+        <p>Verantwortlich nach § 5 TMG:</p>
+
+      <address>
+        <p>Beuth Hochschule für Technik Berlin<br>
+        Luxemburger Straße 10<br>
+        13353 Berlin<br></p><br>
+          
+        <p><strong>Ein Projekt aus Softwareprojekt II - Sommersemester 2016<br></strong>
+        <strong>Betreuer:</strong>
+          Michael<br>
+        <strong>Mitglieder der Gruppe:</strong>
+          Bernd, Günay, Timo, Oliver und Sven
+          
+        </p>
+        <p>GitHub: <a href="https://github.com/mitchobrian/schere-stein-papier/">github.com/mitchobrian/schere-stein-papier</a></p>
+
+      </address>
+
+    </div>
+
+
+    <!-- Beuth Logo
+    ================================================== -->
+    <div class="col-sm-5">
+    <h1></h1>
+        <img src="images/beuth_logo.png" width="300" height="100"> 
+    </div>
+        </div>    
+
+
+    <!-- Haftungsausschluss (Disclaimer)
+    ================================================== -->
+    <div class="col-sm-12">
+      <h2 id="disclaimer">Haftungsausschluss (Disclaimer)</h2>
+        <strong>1. Warnhinweis zu Inhalten</strong>
+      <p>Die kostenlosen und frei zugänglichen Inhalte dieser Webseite wurden mit größtmöglicher Sorgfalt erstellt. Der Anbieter dieser Webseite übernimmt jedoch keine Gewähr für die Richtigkeit und Aktualität der bereitgestellten kostenlosen und frei zugänglichen journalistischen Ratgeber und Nachrichten. Die Nutzung dieser Webseiteninhalte erfolgt auf eigene Gefahr. Allein durch den Aufruf dieser kostenlosen und frei zugänglichen Inhalte kommt keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande, insoweit fehlt es am Rechtsbindungswillen des Anbieters.</p>
+
+        <strong>2. Verlinkungen</strong>
+      <p>Die Webseite enthält Verlinkungen zu anderen Webseiten ("externe Links"). Diese Webseiten unterliegen der Haftung der jeweiligen Seitenbetreiber. Bei Verknüpfung der externen Links waren keine Rechtsverstöße ersichtlich. Auf die aktuelle und künftige Gestaltung der verlinkten Seiten hat der Anbieter keinen Einfluss. Die permanente Überprüfung der externen Links ist für den Anbieter ohne konkrete Hinweise auf Rechtsverstöße nicht zumutbar. Bei Bekanntwerden von Rechtsverstößen werden die betroffenen externen Links unverzüglich gelöscht.</p>    
+
+        <strong>3. Urheberrecht / Leistungsschutzrecht</strong>
+      <p>Die auf dieser Webseite durch den Anbieter veröffentlichten Inhalte unterliegen dem deutschen Urheberrecht und Leistungsschutzrecht. Alle vom deutschen Urheber- und Leistungsschutzrecht nicht zugelassene Verwertung bedarf der vorherigen schriftlichen Zustimmung des Anbieters oder jeweiligen Rechteinhabers. Dies gilt vor allem für Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von Inhalten in Datenbanken oder anderen elektronischen Medien und Systemen. Dabei sind Inhalte und Rechte Dritter als solche gekennzeichnet. Das unerlaubte Kopieren der Webseiteninhalte oder der kompletten Webseite ist nicht gestattet und strafbar. Lediglich die Herstellung von Kopien und Downloads für den persönlichen, privaten und nicht kommerziellen Gebrauch ist erlaubt.
+    Diese Website darf ohne schriftliche Erlaubnis nicht durch Dritte in Frames oder iFrames dargestellt werden.
+    </p>    
+
+        <strong>4. Keine Werbung</strong>
+      <p>Die Verwendung der Kontaktdaten des Impressums zur gewerblichen Werbung ist ausdrücklich nicht erwünscht, es sei denn der Anbieter hatte zuvor seine schriftliche Einwilligung erteilt oder es besteht bereits eine Geschäftsbeziehung. Der Anbieter und alle auf dieser Website genannten Personen widersprechen hiermit jeder kommerziellen Verwendung und Weitergabe ihrer Daten.</p>    
+
+        <strong>5. Besondere Nutzungsbedingungen</strong>
+      <p>Soweit besondere Bedingungen für einzelne Nutzungen dieser Website von den vorgenannten Nummern 1. bis 4. abweichen, wird an entsprechender Stelle ausdrücklich darauf hingewiesen. In diesem Falle gelten im jeweiligen Einzelfall die besonderen Bedingungen.</p>    
+
+
+   <!-- ANDERS EINSETZEN?     
+        <p>Quelle: <a href="http://www.fachanwalt.de/disclaimer-vorlage/">Disclaimer Vorlage</a> powered by <a href="http://www.fachanwalt.de/">fachanwalt.de</a></p>
+-->
+        <strong>6. GNU & GPL Lizenzen:</strong>
+      <p>Die Webseite beinhalten Codes, die von Dritt-Anbietern entwickelt wurden und der GNU General Public License ("GPL") oder GNU Lesser General Public License ("LGPL") unterliegen.</p>
+        
+    </div>
+
+@stop
