38444a3307549e12f9c93aeb729b63c19c958384
Sven <flynn23@gmx.de>
2016-04-11 14:02:38 +0200

    Kleine Ausbesserungen
    - Die master.blade weiter ausgearbeitet
    - Formatierung der Impressum und master blades

--- PHP/Laravel/resources/views/master.blade.php
+++ PHP/Laravel/resources/views/master.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    </head>
-        
-    <body>
-        
-        
-        
-    </body>    
+<html lang="en">
+
+<head>
+    <title>Schnick Schnack Schnuck - @yield('title')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    
+    @yield('content')
+
+</body>
+
 </html>
