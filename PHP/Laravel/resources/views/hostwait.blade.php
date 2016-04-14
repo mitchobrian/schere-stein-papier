906c9e5019266074d190f7e0eee185d61acccd98
gunay58 <gunay65@hotmail.de>
2016-04-14 13:49:41 +0200

    warteseite erstellt
    Platzhalter für username ist drin.
    countdown und share game / share bei facebook etc. folgen

---
+++ PHP/Laravel/resources/views/hostwait.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('title', 'Impressum')
+
+
+@section('content')
+
+    <div class="container-fluid">
+        ---USERNAME--- ,bitte warte, bis dein Partner das Spiel betritt.<br>
+        
+
+    </div>
+
+@stop
