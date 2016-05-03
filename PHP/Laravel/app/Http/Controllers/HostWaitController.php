9af6b821bcb4e3a84b89d04aa2c02054e8105925
gunay58 <gunay65@hotmail.de>
2016-05-03 17:27:06 +0200

    php websocket aufsetzen
    angefangen PHP Websocket aufzusetzen.. ACHTUNG: Hostwaitseite lädt und
    und funktioniert nicht ganz

--- PHP/Laravel/app/Http/Controllers/HostWaitController.php
+++ PHP/Laravel/app/Http/Controllers/HostWaitController.php
@@ -5,11 +5,78 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Routing\Controller;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
 
-class HostWaitController extends Controller
+class HostWaitController extends Controller implements MessageComponentInterface
 {
-    public function index()
-    {
+
+
+    public function index() {
+
         return view('hostwait');
+    }
+
+    /**
+     * When a new connection is opened it will be passed to this method
+     * @param  ConnectionInterface $conn The socket/connection that just connected to your application
+     * @throws \Exception
+     */
+
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    function onOpen(ConnectionInterface $conn)
+    {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    /**
+     * This is called before or after a socket is closed (depends on how it's closed).  SendMessage to $conn will not result in an error if it has already been closed.
+     * @param  ConnectionInterface $conn The socket/connection that is closing/closed
+     * @throws \Exception
+     */
+    function onClose(ConnectionInterface $conn)
+    {
+        // TODO: Implement onClose() method.
+    }
+
+    /**
+     * If there is an error with one of the sockets, or somewhere in the application where an Exception is thrown,
+     * the Exception is sent back down the stack, handled by the Server and bubbled back up the application through this method
+     * @param  ConnectionInterface $conn
+     * @param  \Exception $e
+     * @throws \Exception
+     */
+    function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        // TODO: Implement onError() method.
+    }
+
+    /**
+     * Triggered when a client sends data through the socket
+     * @param  \Ratchet\ConnectionInterface $from The socket/connection that sent the message to your application
+     * @param  string $msg The message received
+     * @throws \Exception
+     */
+    function onMessage(ConnectionInterface $from, $msg)
+    {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
+        }
     }
 }
--- PHP/Laravel/resources/views/hostwait.blade.php
+++ PHP/Laravel/resources/views/hostwait.blade.php
@@ -1,106 +1,123 @@
-@extends('master') 
+@extends('master')
 
-@section('title', 'LandingPage') 
+@section('title', 'LandingPage')
 {{ Html::style('css/bootstrap.min.css') }}
 {{ Html::style('css/style.css') }}
 
 
 @section('content')
-    
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container-fluid" style="margin-bottom: 150px;margin-top:-20px;" align="center">  
-          <h2 style="margin-top:50px;">PLAYER 1 bitte warte, bis dein Partner das Spiel betritt.</h2>
-  <hr>
-       <img src="images/loading.gif" alt="Wartegif" style="height:200px;">       
- <br><br>
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use App\Http\Controllers\HostWaitController;
+
+require 'C:\Bitnami\wampstack-5.6.20-0\apache2\htdocs\schere-stein-papier\PHP\Laravel\vendor\autoload.php';
+
+
+$server = IoServer::factory(
+        new HostWaitController(),
+        8080
+);
+
+$server->run();
+?>
+
+
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <div class="container-fluid" style="margin-bottom: 150px;margin-top:-20px;" align="center">
+        <h2 style="margin-top:50px;">PLAYER 1 bitte warte, bis dein Partner das Spiel betritt.</h2>
+        <hr>
+        <img src="images/loading.gif" alt="Wartegif" style="height:200px;">
+        <br><br>
         <p class="alert alert-info" id="alert" style="width: 800px;">
             <span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span>
             <span class="sr-only">Info:</span>
-            <strong id="time">15:00</strong> <strong>Verschicke diesen Link an einen Freund!</strong>   
-            
-            
+            <strong id="time">15:00</strong> <strong>Verschicke diesen Link an einen Freund!</strong>
+
+
         <div class="input-group">
             <br>
-        <input type="text" class="form-control input-lg txtGameCode" placeholder="Gamecode" value="" style="text-align:center;width: 300px;">
-        <button class="btn btn-primary btn-lg copybtn" type="button">Kopieren</button>
+            <input type="text" class="form-control input-lg txtGameCode" placeholder="Gamecode" value=""
+                   style="text-align:center;width: 300px;">
+            <button class="btn btn-primary btn-lg copybtn" type="button">Kopieren</button>
         </div>
         <!-- /input-group -->
 
-      <div class="social-network" style="text-align:left;">       
-        
-        <img src="images/facebook.png" style="height: 70px;">
-        <img src="images/google-plus.png" style="height: 70px;">
-        <img src="images/twitter.png" style="height: 70px;">
+        <div class="social-network" style="text-align:left;">
 
-      </div>   
-          
-    <script>
-        //Generate Gamecode
-        window.addEventListener('load', function () {
-            var gamecode = document.querySelector('.txtGameCode');
+            <img src="images/facebook.png" style="height: 70px;">
+            <img src="images/google-plus.png" style="height: 70px;">
+            <img src="images/twitter.png" style="height: 70px;">
 
-            var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        </div>
 
-            for( var i=0; i < 6; i++ )
-                text += possible.charAt(Math.floor(Math.random() * possible.length));
+        <script>
+            //Generate Gamecode
+            window.addEventListener('load', function () {
+                var gamecode = document.querySelector('.txtGameCode');
 
-            gamecode.value = window.location.href + "/" + text;
-        })
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        //Countdown
-        function startTimer(duration, display) {
-            var timer = duration, minutes, seconds;
-            setInterval(function () {
-                minutes = parseInt(timer / 60, 10)
-                seconds = parseInt(timer % 60, 10);
+                for (var i = 0; i < 6; i++)
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-                minutes = minutes < 10 ? "0" + minutes : minutes;
-                seconds = seconds < 10 ? "0" + seconds : seconds;
+                gamecode.value = window.location.href + "/" + text;
+            });
 
-                display.textContent = minutes + ":" + seconds;
+            //Countdown
+            function startTimer(duration, display) {
+                var timer = duration, minutes, seconds;
+                setInterval(function () {
+                    minutes = parseInt(timer / 60, 10)
+                    seconds = parseInt(timer % 60, 10);
 
-                if (--timer < 0) {
-                    timer = duration;
-                }
-            }, 1000);
-        }
+                    minutes = minutes < 10 ? "0" + minutes : minutes;
+                    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        window.onload = function () {
-            var fifteenMinutes = 60 * 15,
-                    display = document.querySelector('#time');
-            startTimer(fifteenMinutes, display);
-        };
+                    display.textContent = minutes + ":" + seconds;
 
-
-        //Copy-Funktion
-        var copyTextareaBtn = document.querySelector('.copybtn');
-
-        copyTextareaBtn.addEventListener('click', function (event) {
-            var copyTextarea = document.querySelector('.txtGameCode');
-            copyTextarea.select();
-
-            try {
-                var successful = document.execCommand('copy');
-                var msg = successful ? 'successful' : 'unsuccessful';
-                alert('Der Gamecode wurde kopiert!');
-            } catch (err) {
-                alert('Konnte Gamecode nicht kopieren...');
+                    if (--timer < 0) {
+                        timer = duration;
+                    }
+                }, 1000);
             }
-        });
-    </script>
-          
+
+            window.onload = function () {
+                var fifteenMinutes = 60 * 15,
+                        display = document.querySelector('#time');
+                startTimer(fifteenMinutes, display);
+            };
+
+
+            //Copy-Funktion
+            var copyTextareaBtn = document.querySelector('.copybtn');
+
+            copyTextareaBtn.addEventListener('click', function (event) {
+                var copyTextarea = document.querySelector('.txtGameCode');
+                copyTextarea.select();
+
+                try {
+                    var successful = document.execCommand('copy');
+                    var msg = successful ? 'successful' : 'unsuccessful';
+                    alert('Der Gamecode wurde kopiert!');
+                } catch (err) {
+                    alert('Konnte Gamecode nicht kopieren...');
+                }
+            });
+        </script>
+
     </div>
 </div>
 
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../../public/JS/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../../public/JS/bootstrap.min.js"></script>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="../../public/JS/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../public/JS/bootstrap.min.js"></script>
 
 @stop
