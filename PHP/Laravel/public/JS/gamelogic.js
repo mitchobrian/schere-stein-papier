feab4a62a3d6e9a254517df20ba560c4e6928959
Bernd-Fischer <bernd.noa.fischer@gmail.com>
2016-06-20 16:59:41 +0200

    Gunay Commit:
    - Gamelogik fertig
    - Auswertung fertig/verbessert
    - Countdown auf 30 Sek gesetzt
    - Countdown -1 bug fixed
    - nach 15 sek wird "hurry up" angezeigt
    - Zufallsauswahl button fertig

--- PHP/Laravel/public/JS/gamelogic.js
+++ PHP/Laravel/public/JS/gamelogic.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
+    $('#hurry').hide();
 
     var mySelection
-    console.log(mySelection);
+
+
     //Make selection
     $('#auswahl-div img').on('click', function (e) {
         e.preventDefault();
@@ -19,17 +21,44 @@ $(document).ready(function () {
         }
     })
 
+    $('#btnRandom').on('click', function (e) {
+        e.preventDefault();
+
+        var choices = Array("Schere", "Stein", "Papier");
+        var randomChoice = choices[Math.floor(choices.length * Math.random())];
+
+        //set Selection
+        mySelection = randomChoice;
+
+        //set hiddenbox
+        $('#txtSel').val(mySelection);
+
+        //set Shwon Selection
+        $('#getAuswahl').text(mySelection);
+    })
+
     //Countdown
-    var seconds = 10;
+    var seconds = 30;
     $("#time").html(seconds);
     setInterval(function () {
         seconds--;
         $("#time").html(seconds);
-        if (seconds < 0) {
+
+        //If Countdown is < 15 and nothing chosen
+        if(seconds < 15 && seconds > 9 && mySelection == undefined){
+           $('#hurry').fadeIn(200);
+            if(seconds == 10){
+                $('#hurry').fadeOut(200);
+            }
+        }
+
+        //If Countdown is 0
+        if (seconds < 1) {
             if (mySelection == undefined) {
+                seconds = 0;
                 //If nothing selected
                 //Array with possible choices
-                var randomChoice = Array("Schere","Stein","Papier");
+                var randomChoice = Array("Schere", "Stein", "Papier");
 
                 //Get One Random and put in hidden field as Choice
                 $('#txtSel').val(randomChoice[Math.floor(randomChoice.length * Math.random())]);
@@ -40,6 +69,7 @@ $(document).ready(function () {
                 //Submit Form with Choice
                 $('#choiceForm').submit();
             } else {
+                seconds = 0;
                 //Submit Form with Choice
                 $('#choiceForm').submit();
             }
--- PHP/Laravel/resources/views/gameend.blade.php
+++ PHP/Laravel/resources/views/gameend.blade.php
@@ -31,26 +31,31 @@
             </div>
             <br><br><br>
             <div id="auswahl-div" style="margin-top:-60px;">
-                <div class="auswahl"><img id="schere" src="images/auswahlIcons/schere/gif/links_schere_win.gif"
-                                          style="margin-top:20px;"></div>
+                <div class="auswahl">
+                    <img class="sym" src="images/auswahlIcons/schere/schere_links.png" id="Schere1" style="margin-top:20px;">
+                    <img class="sym" src="images/auswahlIcons/stein/stein_links.png" style="margin-top:20px;" id="Stein1">
+                    <img class="sym" src="images/auswahlIcons/papier/papier_links.png" style="margin-top:20px;" id="Papier1">
+                </div>
                 <div class="auswahl"><span>VS</span></div>
-                <!-- <div class="auswahl"><span>Stein</span></div> -->
-                <div class="auswahl"><img id="papier_broke" src="images/auswahlIcons/papier/gif/papier_rot_lose.gif"
-                                          style="margin-top:20px;" img></div>
+                <div class="auswahl">
+                    <img class="sym" src="images/auswahlIcons/schere/schere_links.png" id="Schere2" style="margin-top:20px;">
+                    <img class="sym" src="images/auswahlIcons/stein/stein_links.png" style="margin-top:20px;" id="Stein2">
+                    <img  class="sym" src="images/auswahlIcons/papier/papier_links.png" style="margin-top:20px;" id="Papier2">
+                </div>
             </div>
 
 
             <!-- Noch mal?-->
-            <div class="container" style="text-align:center;margin-top:-100px;">
+            <div class="container" style="text-align:center;">
                 <div>
                     <h3>Zur Zeit steht es <span id="p1Points">0</span> zu <span id="p2Points">0</span> !</h3>
-                    <a class="btn btn-primary btn-lg" href="{{URL::route('gamepage')}}" style="background:Green;"
+                    <a class="btn btn-primary btn-lg" href="{{URL::route('gamepage')}}"
+                       style="background:Green;"
                        role="button">Nochmal?</a>
                 </div>
             </div>
         </div>
     </div>
-
 
 
     <!-- Bootstrap core JavaScript
