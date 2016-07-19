27162a8c8b8e6cf93040f6e77ed5ee1f84a500d7
Sven <flynn23@gmx.de>
2016-07-19 21:13:29 +0200

    Modal aus der Landingpage entfernt

--- PHP/Laravel/resources/views/landingPage.blade.php
+++ PHP/Laravel/resources/views/landingPage.blade.php
@@ -1,108 +1,76 @@
-@extends('master') 
+@extends('master') @section('title', 'LandingPage') {{ Html::style('css/bootstrap.min.css') }} {{ Html::style('css/style.css') }} @section('content')
 
-@section('title', 'LandingPage') 
-{{ Html::style('css/bootstrap.min.css') }}
-{{ Html::style('css/style.css') }}
-
-
-@section('content')
-    
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container-fluid" style="margin-bottom: 40px;margin-top:-20px;" align="center">
-        <div class="logo-groß"> 
-             <a href=""><img class="img-responsive" src="images/sss_logo_large.png" alt="Movie Places" /></a>
-        </div> 
-        <p style="margin-top:50px;">Spiele Schere-Stein-Papier gegen einen Freund! Schaffst du es auf der Rangliste nach ganz oben?</p>
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <div class="container-fluid" style="margin-bottom: 40px;margin-top:-20px;" align="center">
+        <div class="logo-groß">
+            <a href=""><img class="img-responsive" src="images/sss_logo_large.png" alt="Movie Places" /></a>
+        </div>
+        <p style="margin-top:30px;">Spiele Schere-Stein-Papier gegen einen Freund! Schaffst du es auf der Rangliste nach ganz oben?</p>
         <!-- <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p> -->
-  <hr>
- <br>
- <!-- ANMELDEFORM -->         
- {{--<div class="container" style="text-align:center;">
-  <form class="form-inline" role="form">
-    <div class="form-group">
-      <label class="sr-only" for="Username">Name</label>
-      <input type="Username" class="form-control input-lg" id="Username" placeholder="Username" style="text-align:center;">
-    </div>
-      
-   <!-- <div class="form-group" style="margin-left: 20px;">
+        <hr>
+        <!-- ANMELDEFORM Ertellen-->
+        {{--
+        <div class="container" style="text-align:center;">
+            <form class="form-inline" role="form">
+                <div class="form-group">
+                    <label class="sr-only" for="Username">Name</label>
+                    <input type="Username" class="form-control input-lg" id="Username" placeholder="Username" style="text-align:center;">
+                </div>
+
+                <!-- <div class="form-group" style="margin-left: 20px;">
       <label class="sr-only" for="Gamecode">Spielcode</label>
       <input type="Gamecode" class="form-control input-lg" id="Gamecode" placeholder="Spielcode">
     </div>
     -->
-  </form>
-</div>        --}}
-          
-  
- <!-- BUTTONS--> 
-     <div class="container" style="text-align:center;" >
+            </form>
+        </div> --}}
 
-         <form action="store" method="post">
 
-             <input  type="text" name="name"  placeholder="Gib einen Namen an" required id="name">
+        <!-- BUTTONS-->
+        <div id="rahmen">
+            <!--          Spiel Erstellen-->
+            <div class="container" id="erstellen" style="text-align:center;">
+                <p><b> - Spiel erstellen - </b></p>
+                <form action="store" method="post">
 
-             <input  type="submit" name ="erstellen" id="submit" class="btn btn-primary btn-lg" value="Spiel erstellen!">
+                    <input type="text" style="border:1px solid #000080; height: 3rem" name="name" placeholder=" Gib einen Namen einn" required id="name">
+                    <input type="submit" name="erstellen" id="submit" class="btn btn-primary btn-lg" value="Spiel erstellen!">
+                    <br>
+                    <input type="hidden" name="_token" value="{{ csrf_token()}}">
 
-             <input type="hidden" name="_token" value="{{ csrf_token()}}">
+                </form>
+            </div>
 
-         </form>
-        <div>
-            <a class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal" role="button">Ab ins Spiel!</a>
+            <!--          Spiel Beitreten-->
+
+            <div class="container" id="beitreten" style="text-align:center;">
+                <p><b> - Spiel Beitreten - </b></p>
+                {{--
+                <label class="sr-only" for="Gamecode">Spielcode</label>
+
+                <input type="Gamecode" class="form-control input-lg" id="Gamecode" placeholder="Spielcode" style="width: 400px;">--}}
+                <form action="joinstore" method="post">
+
+
+                    <input style="border:1px solid #000080; height: 3rem" type="text" name="name" id="name" required placeholder=" Gib einen Namen ein">
+                    <input style="border:1px solid #000080; height: 3rem" type="text" name="code" id="code" required maxlength="10" placeholder=" Der erhaltene Code">
+                    <br>
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                    <input type="submit" name="erstellen" id="submit" class="btn btn-primary btn-lg" value="Spiel beitreten!">
+                </form>
+                <div>
+
+                </div>
+            </div>
         </div>
-         {{--
-          <div>
-              <h4><i>Oder</i> </h4>
-
-              <a class="btn btn-primary btn-lg" href="{{URL::route('hostwait')}}" role="button">...erstelle hier ein Spiel!</a>
-          </div>  --}}
-      </div>
-          
-    <!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title">Spielcode</h2>
-      </div>
-      <div class="modal-body">
-        <p>Bitte gebe deinen Spielcode ein, den du erhalten hast!</p>
-       {{-- <label class="sr-only" for="Gamecode">Spielcode</label>
-
-        <input type="Gamecode" class="form-control input-lg" id="Gamecode" placeholder="Spielcode" style="width: 400px;">--}}
-          <form action="joinstore" method="post">
-
-
-              Dein Name  <input style="border:1px solid #000080" type="text" name="name" id="name" required placeholder="Gib einen Namen ein .."><br>
-              Code  <input style="border:1px solid #000080" type="text" name="code" id="code" required maxlength="10" placeholder="Der erhaltene Code"><br><br>
-              <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-              <input  type="submit" name ="erstellen" id="submit" class="btn btn-primary btn-lg" value="Los geht's!">
-          </form>
-
-
-
-
-
-      </div>
-      <div class="modal-footer">
-        {{--<a class="btn btn-primary btn-lg" href="{{URL::route('gamepage')}}" style="margin-right:160px;" role="button">Verbinden</a>--}}
-        <button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
-      </div>
     </div>
-
-  </div>
-</div>
-          
-          
-    </div>
+    <hr>
 </div>
 
 <script type="text/javascript">
-
-    window.onload = function () {
+    window.onload = function() {
 
         $.ajaxSetup({
             headers: {
@@ -123,11 +91,12 @@
 </script>
 
 
-    <!-- Bootstrap core JavaScript
+<!-- Bootstrap core JavaScript
     ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
+<!-- Placed at the end of the document so the pages load faster -->
 {{Html::script('JS/jquery.min.js')}}
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-{{Html::script('JS/bootstrap.min.js')}}
+<script>
+    window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')
 
-@stop
+</script>
+{{Html::script('JS/bootstrap.min.js')}} @stop
